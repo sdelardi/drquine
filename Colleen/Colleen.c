e98771b1615b1a20bdb6a4652501cce103a07e37
Steeve DELARDIERE <sdelardi@e1r4p18.42.fr>
2017-06-01 14:35:30 +0200

    Colleen

---
+++ Colleen/Colleen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/*
+	yo
+*/
+void funct(){};
+int main(){/*tut*/funct();char*c="#include <stdio.h>%c/*%c%cyo%c*/%cvoid funct(){};%cint main(){/*tut*/funct();char*c=%c%s%c;printf(c,10,10,9,10,10,10,34,c,34);}";printf(c,10,10,9,10,10,10,34,c,34);}
