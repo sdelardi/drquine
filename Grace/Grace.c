ca3cab4289f55fc84be2b750c452e16506ce073e
Steeve DELARDIERE <sdelardi@e1r4p18.42.fr>
2017-06-01 19:33:22 +0200

    Grace

---
+++ Grace/Grace.c
@@ -0,0 +1,6 @@
+/*comment*/
+#include <stdio.h>
+#define D1 23
+#define D2 14
+#define DM int main(){FILE*fp=fopen("Grace_kid.c", "w");char*c="/*comment*/%c#include <stdio.h>%c#define D1 23%c#define D2 14%c#define DM int main(){FILE*fp=fopen(%cGrace_kid.c%c, %cw%c);char*c=%c%s%c;fprintf(fp,c,10,10,10,10,34,34,34,34,34,c,34,10);fclose(fp);}%cDM";fprintf(fp,c,10,10,10,10,34,34,34,34,34,c,34,10);fclose(fp);}
+DM
