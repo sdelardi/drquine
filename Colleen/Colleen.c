37af7a6992491cdc3c2a66558bbe4c1686fb9437
Steeve DELARDIERE <sdelardi@e1r4p18.42.fr>
2017-06-02 13:04:49 +0200

    Fix comment

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -3,4 +3,8 @@
 	yo
 */
 void funct(){};
-int main(){/*tut*/funct();char*c="#include <stdio.h>%c/*%c%cyo%c*/%cvoid funct(){};%cint main(){/*tut*/funct();char*c=%c%s%c;printf(c,10,10,9,10,10,10,34,c,34);}";printf(c,10,10,9,10,10,10,34,c,34);}
+int main(){
+/*
+	tut
+*/
+funct();char*c="#include <stdio.h>%c/*%c%cyo%c*/%cvoid funct(){};%cint main(){%c/*%c%ctut%c*/%cfunct();char*c=%c%s%c;printf(c,10,10,9,10,10,10,10,10,9,10,10,34,c,34);}";printf(c,10,10,9,10,10,10,10,10,9,10,10,34,c,34);}
