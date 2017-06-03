74f65db7e252141a2a10850a2190132a1254aa91
Steeve DELARDIERE <sdelardi@e1r4p18.42.fr>
2017-06-03 21:03:52 +0200

    Colleen and Grace ruby

---
+++ bonus/Colleen.rb
@@ -0,0 +1,3 @@
+#first comment
+#second comment
+c = "#first comment%c#second comment%cc = %c%s%c;printf(c, 10, 10, 34, c, 34, 10);%c";printf(c, 10, 10, 34, c, 34, 10);
---
+++ bonus/Grace.rb
@@ -0,0 +1,2 @@
+#comment
+file = File.open("Grace_kid.rb", 'w');c = "#comment%cfile = File.open(%cGrace_kid.rb%c, 'w');c = %c%s%c;file.printf(c, 10, 34, 34, 34, c, 34, 10);file.close();%c";file.printf(c, 10, 34, 34, 34, c, 34, 10);file.close();
